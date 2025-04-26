4958af0234213b72df75b59115b0647b0cf28486
Bhavya <bgprajapati575@gmail.com>
2025-04-26 19:17:24 +0530

    API integration

--- server/nodeserver/routes/user.routes.js
+++ server/nodeserver/routes/user.routes.js
@@ -5,14 +5,11 @@ const { storestatics, getstatistics, uploadFile, upload, giveratingtoteacher, ge
 const {
     verifyUser
 } = require('../middleware/auth.middleware');
-const { createOrder, verifyPayment } = require('../controller/payment.controller');
 
 router.post('/statistics', verifyUser, storestatics);
 router.get('/showhistory', verifyUser, getstatistics);
 router.post('/upload', verifyUser, upload.single('image'), uploadFile);
 router.post('/rating', verifyUser, giveratingtoteacher);
 router.get('/doubthistory', verifyUser, getdoubthistory);
-router.post('/api/payment', verifyUser, createOrder);
-router.post('/api/verifypayment', verifyUser, verifyPayment);
 
 module.exports = router;    
